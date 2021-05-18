1c5d3422416810e3212ec7f4f59869f0d11654f9
Harano0512 <78680327+Harano0512@users.noreply.github.com>
2021-05-18 18:46:14 +0900

    バリデーション実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,21 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable
 
+  with_options presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/} do
+    validates :last_name
+    validates :first_name
+  end
+
+  with_options presence: true, format: {with: /\A[ァ-ヶー－]+\z/} do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
+
+  validates :birthday, presence: true
+  
+  validates :password, presence: true,format: {with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/},confirmation: true
+  validates :email, presence: true
   
 end
