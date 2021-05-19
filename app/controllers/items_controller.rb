2f083b7393b4d1a851d860d4295fac572bae17e7
Harano0512 <78680327+Harano0512@users.noreply.github.com>
2021-05-19 21:03:40 +0900

    ActiveStorage実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:show, :index]
   
   def index
+    @items = Item.all
   end
   
   def new
@@ -9,11 +10,11 @@ class ItemsController < ApplicationController
   end
 
   def create
-    item = Item.create(item_params)
-    if item.save
+    @item = Item.new(item_params)
+    if @item.save
       redirect_to root_path
     else 
-      render "/new"
+      render :new
     end
   end
 
@@ -23,7 +24,7 @@ class ItemsController < ApplicationController
 
   private
   def item_params
-    params.permit(:item_name, :text, :price, :category_id, :status_id, :delivery_charge_id, :prefecture_id, :delivery_days_id)
+    params.require(:item).permit(:item_name, :text, :price, :category_id, :status_id, :delivery_charge_id, :prefecture_id, :delivery_day_id,:image).merge(user_id: current_user.id)
   end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -7,19 +7,22 @@ class Item < ApplicationRecord
   belongs_to :delivery_day
   
 
+  
+  with_options presence: true do
+    validates :item_name
+    validates :text
+    validates :price
+    validates :image
 
-  validates :item_name, presence: true
-  validates :text, presence: true
-  validates :price, presence: true
-
-  validates :category_id, numericality: {other_then: 1}
-  validates :status_id, numericality: {other_then: 1}
-  validates :delivery_charge_id, numericality: {other_then: 1}
-  validates :prefecture_id, numericality: {other_then: 1}
-  validates :delivery_day_id, numericality: {other_then: 1}
-
+    validates :category_id, numericality: {other_than: 1}
+    validates :status_id, numericality: {other_than: 1}
+    validates :delivery_charge_id, numericality: {other_than: 1}
+    validates :prefecture_id, numericality: {other_than: 1}
+    validates :delivery_day_id, numericality: {other_than: 1}
+  end
 
   belongs_to :user
+  has_one_attached :image
 
   
 end
