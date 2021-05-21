ed9c666646f484353470adc31ba2e298a78dbc53
Harano0512 <78680327+Harano0512@users.noreply.github.com>
2021-05-21 11:40:53 +0900

    バリディティとテストコード修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,15 +9,18 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :item_name
     validates :text
-    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
-                      format: { with: /\A[0-9]+\z/ }
     validates :image
+  end
 
-    validates :category_id, numericality: { other_than: 1 }
-    validates :status_id, numericality: { other_than: 1 }
-    validates :delivery_charge_id, numericality: { other_than: 1 }
-    validates :prefecture_id, numericality: { other_than: 1 }
-    validates :delivery_day_id, numericality: { other_than: 1 }
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
+                      format: { with: /\A[0-9]+\z/ }
+
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :status_id
+    validates :delivery_charge_id
+    validates :prefecture_id
+    validates :delivery_day_id
   end
 
   belongs_to :user
