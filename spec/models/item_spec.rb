f2af00bf81954b1fa0bb55363370a203d351c3d0
Harano0512 <78680327+Harano0512@users.noreply.github.com>
2021-05-24 16:30:11 +0900

    rubocop -a実施

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -31,52 +31,52 @@ RSpec.describe Item, type: :model do
       it 'カテゴリーの情報が必須であること' do
         @item.category_id = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include("Category is not a number")
+        expect(@item.errors.full_messages).to include('Category is not a number')
       end
       it 'カテゴリー１が選択された場合出品できないであること' do
         @item.category_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Category must be other than 1")
+        expect(@item.errors.full_messages).to include('Category must be other than 1')
       end
       it '商品の状態についての情報が必須であること' do
         @item.status_id = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include("Status is not a number")
+        expect(@item.errors.full_messages).to include('Status is not a number')
       end
       it '商品の状態について１が選択された場合出品できないであること' do
         @item.status_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Status must be other than 1")
+        expect(@item.errors.full_messages).to include('Status must be other than 1')
       end
       it '配送料の負担についての情報が必須であること' do
         @item.delivery_charge_id = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include("Delivery charge is not a number")
+        expect(@item.errors.full_messages).to include('Delivery charge is not a number')
       end
       it '配送料の負担について１が選択された場合出品できないであること' do
         @item.delivery_charge_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Delivery charge must be other than 1")
+        expect(@item.errors.full_messages).to include('Delivery charge must be other than 1')
       end
       it '発送元の地域についての情報が必須であること' do
         @item.prefecture_id = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include("Prefecture is not a number")
+        expect(@item.errors.full_messages).to include('Prefecture is not a number')
       end
       it '発送元の地域について１が選択された場合出品できないであること' do
         @item.prefecture_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Prefecture must be other than 1")
+        expect(@item.errors.full_messages).to include('Prefecture must be other than 1')
       end
       it '発送までの日数についての情報が必須であること' do
         @item.delivery_day_id = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include("Delivery day is not a number")
+        expect(@item.errors.full_messages).to include('Delivery day is not a number')
       end
       it '発送までの日数について１が選択された場合出品できないであること' do
         @item.delivery_day_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Delivery day must be other than 1")
+        expect(@item.errors.full_messages).to include('Delivery day must be other than 1')
       end
       it '販売価格についての情報が必須であること' do
         @item.price = ''
@@ -89,7 +89,7 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
       end
       it '販売価格は~¥9,999,999のみが保存可能であること' do
-        @item.price = 10000000
+        @item.price = 10_000_000
         @item.valid?
         expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
       end
