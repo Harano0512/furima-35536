2850feed92cfc108046e04c2bbe0d0418ea8b72c
Harano0512 <78680327+Harano0512@users.noreply.github.com>
2021-05-24 16:56:24 +0900

    一覧表示追加実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,7 @@ class ItemsController < ApplicationController
   before_action :check_user, only: :destroy
 
   def index
-    @items = Item.all
+    @items = Item.order(created_at: "DESC")
   end
 
   def new
