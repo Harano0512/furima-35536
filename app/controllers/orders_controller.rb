9b28229ae6dba54ede93b644ef9a743e970fe4f3
Harano0512 <78680327+Harano0512@users.noreply.github.com>
2021-05-26 22:22:42 +0900

    Formオブジェクト、order_address実装・payjp実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,24 +1,28 @@
 class OrdersController < ApplicationController
 
+
   def index
     @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new
   end
 
   def create
     @item = Item.find(params[:item_id])
-    @order = Order.new(order_params)
-    if @order.valid?
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
       pay_item
-      @order.save
-      return redirect_to item_orders(@item.id)
+      binding.pry
+      @order_address.save
+      return redirect_to item_path(@item[:id])
     else
-      render item_orders(@item.id)
+      render :index
     end
   end
 
   private
   def order_params
-    params.require(:order).permit( ).merge(token: params[:token])
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number,
+       :order_id).merge(user_id: current_user.id,item_id: @item.id,token: params[:token])
   end
 
   def pay_item
@@ -26,7 +30,7 @@ class OrdersController < ApplicationController
     Payjp::Charge.create(
       amount: @item.price,
       card: order_params[:token],
-      current: 'jpy'
+      currency: 'jpy'
     )
   end
 
