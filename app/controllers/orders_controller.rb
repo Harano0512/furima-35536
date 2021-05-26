8091ca30857017116418e1af122f6131baf408aa
Harano0512 <78680327+Harano0512@users.noreply.github.com>
2021-05-26 12:45:44 +0900

    PAYJP実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,4 +3,31 @@ class OrdersController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
   end
+
+  def create
+    @item = Item.find(params[:item_id])
+    @order = Order.new(order_params)
+    if @order.valid?
+      pay_item
+      @order.save
+      return redirect_to item_orders(@item.id)
+    else
+      render item_orders(@item.id)
+    end
+  end
+
+  private
+  def order_params
+    params.require(:order).permit( ).merge(token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      current: 'jpy'
+    )
+  end
+
 end
